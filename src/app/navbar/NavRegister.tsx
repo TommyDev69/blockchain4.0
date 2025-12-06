4f76360b4dcbc706115cda8a1121c9e464add972
TommyDev69 <sunnevian4life@gmail.com>
2025-12-07 00:54:39 +0100

    deploy

--- src/app/navbar/NavRegister.tsx
+++ src/app/navbar/NavRegister.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from "next/link"
 // import { useRouter } from "next/router"
 
 import { useRouter } from "next/navigation"
@@ -13,15 +14,15 @@ export default function NavRegister() {
   return (
     <div className='hidden md:flex gap-4 items-center justify-center w-[25%]'>
         <div className="">
-                <button type="button" onClick={() => router.push("/navbar/SignUpProcess")}  className=" rounded-lg borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
+                <Link href="/navbar/SignUpProcess"><button type="button" className=" rounded-lg w-28 borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
                     <p className="capitalize text-white  px-6  py-4 text-[8px] font-extrabold lg:text-[10px] ">sign up</p>
-                </button>
+                </button></Link>
             </div>
 
              <div className="">
-                <button type="button" onClick={() => router.push("/navbar/SignInProcess")} className=" rounded-lg borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
+                <Link href="/navbar/SignInProcess"> <button type="button" className=" rounded-lg w-28 borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
                     <p className="capitalize text-white px-6   py-4 text-[8px] lg:text-[10px] font-extrabold">sign in</p>
-                </button>
+                </button></Link>
             </div>
 
     </div>
