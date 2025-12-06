1d81e5cb1f938d087f942ed3aaa40871aff50a3c
TommyDev69 <sunnevian4life@gmail.com>
2025-12-06 23:07:25 +0100

    deploy

--- src/app/navbar/NavRegister.tsx
+++ src/app/navbar/NavRegister.tsx
@@ -15,7 +15,7 @@ export default function NavRegister() {
             </div>
 
              <div className="">
-                <button type="button"  className=" rounded-lg borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
+                <button type="button" onClick={() => router.push('/signInProcess')} className=" rounded-lg borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
                     <p className="capitalize text-white px-6   py-4 text-[8px] md:text-[10px] font-extrabold">sign in</p>
                 </button>
             </div>
