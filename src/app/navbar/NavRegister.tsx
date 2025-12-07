d6850a3f8375b27e40d41001fff09a37b4adab05
TommyDev69 <sunnevian4life@gmail.com>
2025-12-07 01:03:35 +0100

    deploy

--- src/app/navbar/NavRegister.tsx
+++ src/app/navbar/NavRegister.tsx
@@ -15,13 +15,13 @@ export default function NavRegister() {
     <div className='hidden md:flex gap-2 items-center justify-center w-[40%] pl-16 pr-2'>
         <div className="">
                 <Link href="/navbar/SignUpProcess"><button type="button" className=" rounded-lg w-28 borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
-                    <p className="capitalize text-white  px-6  py-4 text-[8px] font-extrabold lg:text-[10px] ">sign up</p>
+                    <p className="capitalize text-white  px-4  py-4 text-[8px] font-extrabold lg:text-[10px] ">sign up</p>
                 </button></Link>
             </div>
 
              <div className="">
                 <Link href="/navbar/SignInProcess"> <button type="button" className=" rounded-lg w-28 borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
-                    <p className="capitalize text-white px-6   py-4 text-[8px] lg:text-[10px] font-extrabold">sign in</p>
+                    <p className="capitalize text-white px-4   py-4 text-[8px] lg:text-[10px] font-extrabold">sign in</p>
                 </button></Link>
             </div>
 
