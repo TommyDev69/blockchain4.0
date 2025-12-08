3a23f36f0469f560e95315043d1bf97a3da77a4b
TommyDev69 <sunnevian4life@gmail.com>
2025-12-08 17:42:58 +0100

    deploy

--- src/app/navbar/MenuNav.tsx
+++ src/app/navbar/MenuNav.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Brand from "../../../public/images/logo/logo.svg"
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function MenuNav() {
       const router = useRouter();
@@ -25,15 +26,19 @@ export default function MenuNav() {
 
                 <div className=' flex-rows bg-ambewr-600 gap-4 items-center justify-center space-y-4 '>
                     <div className="max-w-full">
-                            <button type="button"onClick={() => router.push('./navbar/signUpProcess')}  className=" w-full rounded-lg borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
-                                <p className="capitalize text-white  px-6  py-4 text-[8px] font-extrabold md:text-[10px]">sign up</p>
-                            </button>
+                            <Link href="/navbar/SignUpProcess">
+                                <button type="button"onClick={() => router.push('./navbar/signUpProcess')}  className=" w-full rounded-lg borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
+                                    <p className="capitalize text-white  px-6  py-4 text-[8px] font-extrabold md:text-[10px]">sign up</p>
+                                </button>
+                            </Link>
                     </div>
 
                         <div className="max-w-full">
+                            <Link href="/navbar/SignInProcess"> 
                             <button type="button" className=" w-full rounded-lg borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
                                 <p className="capitalize text-white px-6   py-4 text-[8px] md:text-[10px] font-extrabold">sign in</p>
                             </button>
+                            </Link>
                         </div>
 
                 </div>
