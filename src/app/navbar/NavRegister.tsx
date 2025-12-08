110588cb76c2adcd96ea5ef42f5436c9ebef47d8
TommyDev69 <sunnevian4life@gmail.com>
2025-12-08 16:58:25 +0100

    deploy

--- src/app/navbar/NavRegister.tsx
+++ src/app/navbar/NavRegister.tsx
@@ -1,19 +1,14 @@
 'use client'
-
-import { useRouter } from "next/navigation"
-
-
 import Link from "next/link"
 
-
 export default function NavRegister() {
-    const router = useRouter();
+    
     
 
-  const handleClick = () => {
-    router.push("/navbar/SignInProcess") // Navigate to a page
-    console.log('/navbar/SignInProcess')
-  }
+  // const handleClick = () => {
+  //   router.push("/navbar/SignInProcess") // Navigate to a page
+  //   console.log('/navbar/SignInProcess')
+
 
  
 
@@ -24,11 +19,10 @@ export default function NavRegister() {
                     <p className="capitalize text-white px-4  py-4 text-[8px] font-extrabold lg:text-[10px] ">sign up</p>
                 </button></Link>
         </div>
-
              <div className="">
-                <button type="button" onClick={handleClick} className=" rounded-lg w-24 borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
+                <Link href="/navbar/SignUpProcess"><button type="button"  className=" rounded-lg w-24 borwder-2 border-[rgba(189, 36, 223, 10)] bg-[linear-gradient(90deg,rgba(189,36,223,0.9),rgba(45,106,222,0.9)_97.15%)] ;">
                     <p className="capitalize text-white px-4   py-4 text-[8px] lg:text-[10px] font-extrabold">sign in</p>
-                </button>
+                </button></Link>
             </div>
 
     </div>
